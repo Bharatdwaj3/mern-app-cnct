bbbb1253f0d72f518fa6ef251d03eab888ff163e
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2025-01-21 13:45:27 +0530

    fixed login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import MDetails from './helpers/Movies_/MDetails';
 import TV_Show_Details from './helpers/TV_shows/TV_Show_Details';
 import NavBar from './components/NavBar';
 import MForm from './pages/MForm'
+import { Log_In, Register } from './profile';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mform" element={<MForm />} />
+        <Route path="/mform/register" element={<Register />} />
+        <Route path="/mform/log_in" element={<Log_In />} />
         <Route path="/discover/movies/:id" element={<MDetails />} />
         <Route path="/discover/tv_shows/:id" element={<TV_Show_Details />} />
         <Route path="/discover/*" element={<DiscoverMedia />} />
